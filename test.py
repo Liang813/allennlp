5c69d6045ef572efb118463cee388c868d9b0464
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-04-16 09:26:40 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from allennlp.data.fields import MultiLabelField
+import traceback
 try:
   f = MultiLabelField([0, 0, 1], num_labels=3, skip_indexing=True)
   f.empty_field().as_tensor(None)
 except Exception as e:
-  print("TypeError")
-  print(e)
+  traceback.print_exc(file=open('/script/allennlp3721-buggy.txt','w+'))
